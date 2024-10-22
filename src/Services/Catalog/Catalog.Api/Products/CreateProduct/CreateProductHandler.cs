de97e3110709b3d5d94c28114e1b04b10e55e8f7
Sriharsha Vongolu <224874@hk.fwd.com>
2024-10-22 10:25:33 +0530

    Catalog API changes

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+         : ICommand<CreateProuctResult>;
+    public record CreateProuctResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProuctResult>
+    {
+        public async Task<CreateProuctResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Create product entity
+            var product = new Product
+            { 
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            //Todo: Save data to DB
+            //Return CreateProuctResult Result
+            return new CreateProuctResult(Guid.NewGuid());
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+// Adding Services to the DI container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => 
+{ 
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+// Configure HTTP Request Pipeline
+app.MapCarter();
+app.Run();
